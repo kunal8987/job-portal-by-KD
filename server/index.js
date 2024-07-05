169eb3c837b7f4e56d86c348841e8636861b8706
Kunal Deotale <kdeotale4@gmail.com>
2024-07-06 02:47:59 +0530

    create basic setup

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+
+dotenv.config();
+
+let app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+let port = process.env.JOB_PORT || 4500;
+app.listen(port, async (req, res) => {
+    try {
+        await connectDB();
+        console.log("Server Is Listing On Port", port);
+    } catch (error) {
+        console.error("An Error Occurred While Starting The Server");
+        console.log(error.message);
+    }
+});
