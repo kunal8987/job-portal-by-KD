6bc80d1b953537b343af2b33c3372c0e7fb8d0ed
Kunal Deotale <kdeotale4@gmail.com>
2024-07-17 00:23:17 +0530

    setup the app

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className=" container font-lora text-3xl font-bold underline">
       Hello world!
     </h1>
   );
